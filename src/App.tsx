12a45e6007b6e246c6b26edbaf9ecec2760fc8a2
Murilo <dixmurilox@gmail.com>
2024-08-01 16:19:10 -0300

    refactor: Update App component to use React hooks for managing city, weather, and forecast state

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,54 @@ import { Title } from "./AppStyles";
 
 function App() {
   const [city, setCity] = useState("");
+  const [state, setState] = useState("");
   const [weather, setWeather] = useState(null);
   const [forecast, setForecast] = useState([]);
+
+  const apiKey = import.meta.env.VITE_API_KEY || "";
+
+  async function getCoordinates(city: string) {
+    try {
+      const response = await axios.get(
+        `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${apiKey}`,
+      );
+      const data = response.data;
+      const { lat, lon, state, name } = data[0];
+      return {
+        lat,
+        lon,
+        state,
+        name,
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function searchWeather() {
+    const data = await getCoordinates(city);
+    if (!data) return;
+    const { name, state, lat, lon } = data;
+    setCity(name);
+    setState(state);
+
+    try {
+      const response = await axios.get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`,
+      );
+      const weather = response.data;
+      setWeather(weather);
+      console.log(weather);
+    } catch (error) {
+      console.log("Erro:", error);
+    }
+  }
 
   return (
     <>
       <Title>Condições Climaticas</Title>
-      <Search />
+      <h3></h3>
+      <Search city={city} setCity={setCity} searchWeather={searchWeather} />
       <CurrentWeather />
       <Forecast />
     </>
