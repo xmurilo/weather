15bab88e76be26c09ab0dc939c9be405eb7708e4
Murilo <dixmurilox@gmail.com>
2024-08-01 20:27:49 -0300

    Refactor types.ts to include interfaces for weather data

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,3 +1,10 @@
+// src/types.ts
+
+export interface Coord {
+  lat: number;
+  lon: number;
+}
+
 export interface Weather {
   id: number;
   main: string;
@@ -11,39 +18,77 @@ export interface Main {
   temp_min: number;
   temp_max: number;
   pressure: number;
+  sea_level?: number;
+  grnd_level?: number;
   humidity: number;
-  sea_level: number;
-  grnd_level: number;
+  temp_kf?: number;
 }
 
 export interface Wind {
   speed: number;
   deg: number;
+  gust?: number;
+}
+
+export interface Clouds {
+  all: number;
 }
 
 export interface Sys {
   country: string;
   sunrise: number;
   sunset: number;
+  pod?: string;
+}
+
+export interface Rain {
+  "3h"?: number;
 }
 
 export interface WeatherData {
-  coord: {
-    lat: number;
-    lon: number;
-  };
+  coord: Coord;
   weather: Weather[];
   base: string;
   main: Main;
   visibility: number;
   wind: Wind;
-  clouds: {
-    all: number;
-  };
+  clouds: Clouds;
   dt: number;
   sys: Sys;
   timezone: number;
   id: number;
   name: string;
   cod: number;
+}
+
+export interface City {
+  coord: Coord;
+  country: string;
+  id: number;
+  name: string;
+  population: number;
+  sunrise: number;
+  sunset: number;
+  timezone: number;
+}
+
+export interface ForecastListItem {
+  dt: number;
+  main: Main;
+  weather: Weather[];
+  clouds: Clouds;
+  wind: Wind;
+  visibility: number;
+  pop: number;
+  rain?: Rain;
+  sys: Sys;
+  dt_txt: string;
+}
+
+export interface ForecastData {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: ForecastListItem[];
+  city: City;
 }
