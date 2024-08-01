f6a7328108c9fda266f29b511131afb4500d593e
Murilo <dixmurilox@gmail.com>
2024-08-01 15:06:33 -0300

    Refactor App component to use React hooks for managing city, weather, and forecast state

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
+import axios from "axios";
+
+// * Components
 import Search from "./components/Search";
 import CurrentWeather from "./components/CurrentWeather";
 import Forecast from "./components/Forecast";
+
+// * StyledComponents
 import { Title } from "./AppStyles";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [forecast, setForecast] = useState([]);
+
   return (
     <>
       <Title>Condições Climaticas</Title>
