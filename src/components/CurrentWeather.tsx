e1976d031c35102ba87a82205a3eb7fbcb20478e
Murilo <dixmurilox@gmail.com>
2024-08-01 15:06:21 -0300

    feat: Update CurrentWeather component with necessary elements for displaying weather information

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,11 +1,12 @@
-
-
 const CurrentWeather = () => {
   return (
     <div>
-      CurrentWeather
+      <h3>Nome</h3>
+      <img src="" alt="" />
+      <p>ºC</p>
+      <p>Descrição da temperatura</p>
     </div>
-  )
-}
+  );
+};
 
-export default CurrentWeather
+export default CurrentWeather;
