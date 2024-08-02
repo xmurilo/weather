f35f38bcf175593e5cf14019c7ea5a3e4a82e986
Murilo <dixmurilox@gmail.com>
2024-08-02 01:03:24 -0300

    refactor: Update AppStyles to include WeatherContainer for consistent styling and improved maintainability

--- src/AppStyles.ts
+++ src/AppStyles.ts
@@ -4,3 +4,13 @@ export const Title = styled.h2`
   font-size: 2rem;
   margin-bottom: 20px;
 `;
+
+export const WeatherContainer = styled.div`
+  background: linear-gradient(to bottom, #89cff0, #005c99);
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
