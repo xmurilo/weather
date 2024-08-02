154a399f91200515fe9c73094576fa3bf58f3f0f
Murilo <dixmurilox@gmail.com>
2024-08-01 21:39:31 -0300

    Refactor Forecast component to use props for forecasts and render each forecast item

--- src/components/Forecast.tsx
+++ src/components/Forecast.tsx
@@ -1,12 +1,25 @@
-const Forecast = () => {
+import { ForecastListItem } from "../types/types";
+
+interface ForecastProps {
+  forecasts: ForecastListItem[];
+}
+
+const Forecast: React.FC<ForecastProps> = ({ forecasts }) => {
+  console.log(forecasts);
+
   return (
     <div>
       <h4>Previão para as próximas horas</h4>
       <ul>
-        <li>
-          <img src="" alt="" />
-          ºC - Descrição 
-        </li>
+        {forecasts.map(forecast => (
+          <li key={forecast.dt}>
+            <img
+              src={`http://openweathermap.org/img/wn/${forecast.weather[0].icon}.png`}
+              alt={forecast.weather[0].description}
+            />
+            {forecast.main.temp}ºC - {forecast.weather[0].description}
+          </li>
+        ))}
       </ul>
     </div>
   );
