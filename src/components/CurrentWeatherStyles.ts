24ae564bfe60393791557b73095fab73c1c62334
Murilo <dixmurilox@gmail.com>
2024-08-02 01:04:06 -0300

    style: Add styled-components for CurrentWeatherStyles component

---
+++ src/components/CurrentWeatherStyles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const InfoWeather = styled.div`
+  text-align: center;
+
+  h3 {
+    font-size: 1.5rem;
+    margin-bottom: 10px;
+  }
+  img {
+    width: 80px;
+    height: 80px;
+
+    p {
+      margin-bottom: 5px;
+    }
+  }
+`;
